22b4226aabea0fdc9d9734936b42abc515dd5ab8
profmaad <prof.maad@lambda-bb.de>
2014-04-12 15:56:33 +0800

    Start implementing basic sinatra-based JSON API

---
+++ server/api.rb
@@ -0,0 +1,102 @@
+require 'sinatra'
+require 'mongoid'
+require 'json'
+#require 'sinatra/reloader' if development?
+
+require 'pp'
+
+puts File.expand_path(File.dirname(__FILE__) + '/../client')
+set :public_folder, File.expand_path(File.dirname(__FILE__) + '/../client')
+
+require_relative 'models/option'
+
+Mongoid.load!('mongoid.yml')
+
+if(Option.all.size == 0)
+  Option.create([
+                 {
+                   type: 'european',
+                   timestamp: Time.now,
+                   strike_price: 100.0,
+                   maturity: 3.0,
+                   risk_free_rate: 5.0,
+                   start_prices: [100],
+                   volatilities: [30],
+                   number_of_assets: 1,
+                   correlations: [],
+                   price: 8.23,
+                   confidence_interval: [8.22, 8.24],
+                   samples: 10000000,
+                 },
+                 {
+                   type: 'asian_arithmetic',
+                   timestamp: Time.now,
+                   strike_price: 100.0,
+                   maturity: 3.0,
+                   risk_free_rate: 5.0,
+                   averaging_steps: 50,
+                   start_prices: [100],
+                   volatilities: [30],
+                   number_of_assets: 1,
+                   correlations: [],
+                   price: 14.547,
+                   confidence_interval: [14.5, 14.6],
+                   samples: 10000000,
+                 },
+                 {
+                   type: 'basket_arithmetic',
+                   timestamp: Time.now,
+                   strike_price: 100.0,
+                   maturity: 3.0,
+                   risk_free_rate: 5.0,
+                   start_prices: [100, 90, 110],
+                   volatilities: [30, 15, 10],
+                   number_of_assets: 3,
+                   correlations: [[1.0, 0.8, 0.5], [0.8, 1, 0.3], [0.5, 0.3, 1]],
+                   price: 23.4242,
+                   confidence_interval: [23.4, 23.44],
+                   samples: 10000000,
+                 },
+                ])
+end
+
+get '/api/options', :provides => :json do
+  content_type :json
+
+  options = Option.desc(:timestamp)
+
+  return {options: options}.to_json
+end
+get '/api/options/:id', :provides => :json do
+  content_type :json
+
+  option = Option.find(params[:id])
+
+  return {option: option}.to_json
+end
+
+put '/api/options/:id', :provides => :json do
+  content_type :json
+
+  pp params
+end
+
+post '/api/options', :provides => :json do
+  content_type :json
+
+  json = JSON.parse(request.body.read)
+
+  option = json['option']
+  option['timestamp'] = Time.at(option['timestamp'].to_i/1000)
+
+  new_option = Option.create(option)
+
+  return {success: true, option: {_id: new_option._id}}.to_json
+end
+
+delete '/api/options/:id', :provides => :json do
+  content_type :json
+
+
+  Option.find(params[:id]).delete
+end
---
+++ server/models/option.rb
@@ -0,0 +1,23 @@
+class Option
+  include Mongoid::Document
+
+  field :type, type: String
+  field :direction, type: String
+  field :control_variate, type: String
+  field :timestamp, type: DateTime
+
+  field :strike_price, type: Float
+  field :maturity, type: Float
+  field :risk_free_rate, type: Float
+
+  field :start_prices, type: Array
+  field :volatilities, type: Array
+  field :correlations, type: Array
+
+  field :number_of_asset, type: Integer
+
+  field :price, type: Float
+  field :confidence_interval, type: Array
+
+  field :samples, type: Integer
+end
